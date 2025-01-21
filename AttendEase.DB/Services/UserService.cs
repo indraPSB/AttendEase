2dcff0b69098b7ec7717396e52b158ac8fb74c70
Indrani <indranipandian@gmail.com>
2025-01-21 11:31:01 +0800

    Remove logger references as logger shall be injected

--- AttendEase.DB/Services/UserService.cs
+++ AttendEase.DB/Services/UserService.cs
@@ -7,19 +7,7 @@ namespace AttendEase.DB.Services;
 
 internal static class UserService
 {
-    private static ILogger _logger;
-
-    static UserService()
-    {
-        ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
-        {
-            builder.AddConsole();
-            builder.AddDebug();
-        });
-        _logger = loggerFactory.CreateLogger(nameof(UserService));
-    }
-
-    public static async Task<IEnumerable<User>?> GetUsers(AttendEaseDbContext context, CancellationToken ct = default)
+    public static async Task<IEnumerable<User>?> GetUsers<T>(ILogger<T> logger, AttendEaseDbContext context, CancellationToken ct = default)
     {
         try
         {
@@ -27,13 +15,13 @@ internal static class UserService
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Error in GetUsers with message, '{message}'.", ex.Message);
+            logger.LogError(ex, "Error in GetUsers with message, '{message}'.", ex.Message);
         }
 
         return null;
     }
 
-    public static async Task<User?> GetUser(AttendEaseDbContext context, Guid id, CancellationToken ct = default)
+    public static async Task<User?> GetUser<T>(ILogger<T> logger, AttendEaseDbContext context, Guid id, CancellationToken ct = default)
     {
         try
         {
@@ -41,7 +29,7 @@ internal static class UserService
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Error in GetUser with message, '{message}'.", ex.Message);
+            logger.LogError(ex, "Error in GetUser with message, '{message}'.", ex.Message);
         }
 
         return null;
