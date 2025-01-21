a112d42d57cf4db4201b926954b406b6fd1b20b9
Indrani <indranipandian@gmail.com>
2025-01-21 10:37:32 +0800

    Interface for user service

---
+++ AttendEase.Shared/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using AttendEase.DB.Models;
+
+namespace AttendEase.Shared.Services;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> GetUsers(CancellationToken ct = default);
+
+    Task<User> GetUser(Guid id, CancellationToken ct = default);
+}
