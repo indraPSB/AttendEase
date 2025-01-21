d5268ced3bd09e3a68f248c9cb65417a287222eb
Indrani <indranipandian@gmail.com>
2025-01-21 08:26:36 +0800

    Scaffold DB context & model

---
+++ AttendEase.DB/Contexts/AttendEaseDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using AttendEase.DB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AttendEase.DB.Contexts;
+
+public partial class AttendEaseDbContext : DbContext
+{
+    public AttendEaseDbContext()
+    {
+    }
+
+    public AttendEaseDbContext(DbContextOptions<AttendEaseDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.ToTable("user");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Name).HasColumnName("name");
+            entity.Property(e => e.Password).HasColumnName("password");
+            entity.Property(e => e.Role).HasColumnName("role");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ AttendEase.DB/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AttendEase.DB.Models;
+
+public partial class User
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Role { get; set; }
+}
