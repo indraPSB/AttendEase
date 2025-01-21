59d2997f785bd1107b0eba7623f0b4d585258f63
Indrani <indranipandian@gmail.com>
2025-01-21 11:12:27 +0800

    Implement user service for DB

---
+++ AttendEase.DB/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using AttendEase.DB.Contexts;
+using AttendEase.DB.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace AttendEase.DB.Services;
+
+internal static class UserService
+{
+    private static ILogger _logger;
+
+    static UserService()
+    {
+        ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+            builder.AddDebug();
+        });
+        _logger = loggerFactory.CreateLogger(nameof(UserService));
+    }
+
+    public static async Task<IEnumerable<User>?> GetUsers(AttendEaseDbContext context, CancellationToken ct = default)
+    {
+        try
+        {
+            return await context.Users.ToListAsync(ct);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in GetUsers with message, '{message}'.", ex.Message);
+        }
+
+        return null;
+    }
+
+    public static async Task<User?> GetUser(AttendEaseDbContext context, Guid id, CancellationToken ct = default)
+    {
+        try
+        {
+            return await context.Users.SingleOrDefaultAsync(u => u.Id == id, ct);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in GetUser with message, '{message}'.", ex.Message);
+        }
+
+        return null;
+    }
+}
