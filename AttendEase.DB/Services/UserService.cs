493a85e823186dd2799009d75e38733d6ed5b686
Indrani <indranipandian@gmail.com>
2025-01-26 03:57:00 +0800

    Check for default CancellationToken and set to None

--- AttendEase.DB/Services/UserService.cs
+++ AttendEase.DB/Services/UserService.cs
@@ -9,13 +9,18 @@ internal static class UserService
 {
     public static async Task<IEnumerable<User>?> GetUsers<T>(ILogger<T> logger, AttendEaseDbContext context, CancellationToken ct = default)
     {
+        if (ct == default)
+        {
+            ct = CancellationToken.None;
+        }
+
         try
         {
             return await context.Users.ToListAsync(ct);
         }
         catch (Exception ex)
         {
-            logger.LogError(ex, "Error in GetUsers with message, '{message}'.", ex.Message);
+            logger.LogError(ex, "Error in DB GetUsers with message, '{message}'.", ex.Message);
         }
 
         return null;
@@ -23,13 +28,18 @@ internal static class UserService
 
     public static async Task<User?> GetUser<T>(ILogger<T> logger, AttendEaseDbContext context, Guid id, CancellationToken ct = default)
     {
+        if (ct == default)
+        {
+            ct = CancellationToken.None;
+        }
+
         try
         {
             return await context.Users.SingleOrDefaultAsync(u => u.Id == id, ct);
         }
         catch (Exception ex)
         {
-            logger.LogError(ex, "Error in GetUser with message, '{message}'.", ex.Message);
+            logger.LogError(ex, "Error in DB GetUser with message, '{message}'.", ex.Message);
         }
 
         return null;
