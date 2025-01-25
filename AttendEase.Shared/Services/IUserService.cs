b8b3732b0c953814e629ee47f89f33a1496059cd
Indrani <indranipandian@gmail.com>
2025-01-26 04:36:34 +0800

    Implement user shared services

--- AttendEase.Shared/Services/IUserService.cs
+++ AttendEase.Shared/Services/IUserService.cs
@@ -1,4 +1,6 @@
 ﻿using AttendEase.DB.Models;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Json;
 
 namespace AttendEase.Shared.Services;
 
@@ -7,4 +9,76 @@ public interface IUserService
     Task<IEnumerable<User>?> GetUsers(CancellationToken ct = default);
 
     Task<User?> GetUser(Guid id, CancellationToken ct = default);
+}
+
+public class UserService(ILogger<UserService> logger, HttpClient httpClient) : IUserService
+{
+    private readonly ILogger<UserService> _logger = logger;
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<IEnumerable<User>?> GetUsers(CancellationToken ct = default)
+    {
+        if (ct == default)
+        {
+            ct = CancellationToken.None;
+        }
+
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("/api/users");
+            if (response.IsSuccessStatusCode)
+            {
+                IEnumerable<User>? users = await response.Content.ReadFromJsonAsync<IEnumerable<User>>(cancellationToken: ct);
+
+                if (users is null)
+                {
+                    _logger.LogWarning("No users found.");
+                }
+                else
+                {
+                    _logger.LogInformation("Users retrieved successfully.");
+                }
+
+                return users;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in Shared GetUsers with message, '{message}'.", ex.Message);
+        }
+        return null;
+    }
+
+    public async Task<User?> GetUser(Guid id, CancellationToken ct = default)
+    {
+        if (ct == default)
+        {
+            ct = CancellationToken.None;
+        }
+
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"/api/users/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                User? user = await response.Content.ReadFromJsonAsync<User>(cancellationToken: ct);
+
+                if (user is null)
+                {
+                    _logger.LogWarning("No user found.");
+                }
+                else
+                {
+                    _logger.LogInformation("User retrieved successfully.");
+                }
+
+                return user;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in Shared GetUser with message, '{message}'.", ex.Message);
+        }
+        return null;
+    }
 }
