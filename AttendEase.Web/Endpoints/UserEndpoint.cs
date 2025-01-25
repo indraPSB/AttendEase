06474625a39dfef898ef2f3f9660b8ca1a158bd6
Indrani <indranipandian@gmail.com>
2025-01-26 03:53:41 +0800

    Add endpoint for user services

---
+++ AttendEase.Web/Endpoints/UserEndpoint.cs
@@ -0,0 +1,40 @@
+﻿using AttendEase.DB.Models;
+using AttendEase.Shared.Services;
+
+namespace AttendEase.Web.Endpoints;
+
+internal static class UserEndpoint
+{
+    public static WebApplication MapUserEndpoints(this WebApplication app)
+    {
+        app.MapGet("api/users", GetUsers);
+
+        app.MapGet("api/users/{id:guid}", GetUser);
+
+        return app;
+    }
+
+    public static async Task<IResult> GetUsers(IUserService userService)
+    {
+        IEnumerable<User>? users = await userService.GetUsers();
+
+        if (users is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(users);
+    }
+
+    public static async Task<IResult> GetUser(IUserService userService, Guid id)
+    {
+        User? user = await userService.GetUser(id);
+
+        if (user is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(user);
+    }
+}
