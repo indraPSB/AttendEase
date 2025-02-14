16c6a39ad39078d08b36f44c3ad0410150f9e519
Indrani <indranipandian@gmail.com>
2025-02-14 19:06:10 +0800

    Register configuration in MAUI

--- AttendEase/MauiProgram.cs
+++ AttendEase/MauiProgram.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace AttendEase
 {
@@ -14,14 +15,23 @@ namespace AttendEase
         {
             var builder = MauiApp.CreateBuilder();
 
-            builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
+
+            // Load the appsettings.json file from the embedded resources
+            using Stream? stream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("AttendEase.appsettings.json")
+                ?? throw new FileNotFoundException("'appsettings.json' not found in embedded resources.");
+
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonStream(stream)
+                .Build();
+
+            builder.Configuration.AddConfiguration(config);
 
             builder.Services.AddAuthorizationCore();
 
