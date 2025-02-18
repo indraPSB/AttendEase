f1f0b03e9982f4962d5026f5fb5628c757f3f9fd
Indrani <indranipandian@gmail.com>
2025-02-18 23:18:24 +0800

    Add DB ScheduleService

---
+++ AttendEase.DB/Services/ScheduleService.cs
@@ -0,0 +1,47 @@
+﻿using AttendEase.DB.Contexts;
+using AttendEase.DB.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace AttendEase.DB.Services;
+
+internal static class ScheduleService
+{
+    public static async Task<IEnumerable<Schedule>?> GetSchedules<T>(ILogger<T> logger, AttendEaseDbContext context, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        try
+        {
+            return await context.Schedules.ToListAsync(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error in DB GetSchedule with message, '{message}'.", ex.Message);
+        }
+
+        return null;
+    }
+
+    public static async Task<Schedule?> GetSchedule<T>(Guid id, ILogger<T> logger, AttendEaseDbContext context, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        try
+        {
+            return await context.Schedules.SingleOrDefaultAsync(u => u.Id == id, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error in DB GetSchedule(Id) with message, '{message}'.", ex.Message);
+        }
+
+        return null;
+    }
+}
