d43142586b8d779448fcde94d259d47f7fe4b818
Indrani <indranipandian@gmail.com>
2025-02-21 22:16:23 +0800

    Add ILocationService

---
+++ AttendEase.Shared/Services/ILocationService.cs
@@ -0,0 +1,8 @@
+﻿namespace AttendEase.Shared.Services;
+
+public interface ILocationService
+{
+    Task<Location> GetCurrentLocation();
+}
+
+public record Location(double Latitude, double Longitude);
