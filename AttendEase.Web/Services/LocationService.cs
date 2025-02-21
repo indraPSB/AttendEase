f3cd549e3a875389a758b3ce3e4655f8f103c5c2
Indrani <indranipandian@gmail.com>
2025-02-21 22:32:53 +0800

    Implement Web LocationService

---
+++ AttendEase.Web/Services/LocationService.cs
@@ -0,0 +1,15 @@
+﻿using AttendEase.Shared.Services;
+using Microsoft.JSInterop;
+
+namespace AttendEase.Web.Services;
+
+public class LocationService(IJSRuntime jSRuntime) : ILocationService
+{
+    private readonly IJSRuntime _jSRuntime = jSRuntime;
+
+    public async Task<Location> GetCurrentLocation()
+    {
+        await Task.Yield();
+        return new Location(0, 0);
+    }
+}
