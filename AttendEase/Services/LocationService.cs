05d063b7402eed464bf8f85b7ca880cf94564068
Indrani <indranipandian@gmail.com>
2025-02-21 22:23:41 +0800

    Implement MAUI LocationService

---
+++ AttendEase/Services/LocationService.cs
@@ -0,0 +1,48 @@
+﻿using AttendEase.Shared.Services;
+using Microsoft.Extensions.Logging;
+using DeviceLocation = Microsoft.Maui.Devices.Sensors.Location;
+using ServiceLocation = AttendEase.Shared.Services.Location;
+
+namespace AttendEase.Services;
+
+internal class LocationService(ILogger<LocationService> logger) : ILocationService
+{
+    private readonly ILogger<LocationService> _logger = logger;
+
+    public async Task<ServiceLocation> GetCurrentLocation()
+    {
+        DeviceLocation? location = null;
+
+        try
+        {
+            await Geolocation.GetLastKnownLocationAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to get last known location.");
+        }
+
+        if (location is null)
+        {
+            try
+            {
+                location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                {
+                    DesiredAccuracy = GeolocationAccuracy.Medium,
+                    Timeout = TimeSpan.FromSeconds(30)
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get location.");
+            }
+        }
+
+        if (location is null)
+        {
+            return new ServiceLocation(0, 0);
+        }
+
+        return new ServiceLocation(location.Latitude, location.Longitude);
+    }
+}
