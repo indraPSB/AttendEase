623ce77c7fd13ab533130899689bd40a1cc0e67b
Indrani <indranipandian@gmail.com>
2025-02-24 20:35:41 +0800

    Add IAttendanceService

---
+++ AttendEase.Shared/Services/AttendanceService.cs
@@ -0,0 +1,16 @@
+﻿using AttendEase.DB.Models;
+
+namespace AttendEase.Shared.Services;
+
+public interface IAttendanceService
+{
+    Task<Attendance?> GetAttendance(GetAttendanceRequest id, CancellationToken cancellationToken = default);
+}
+
+public class GetAttendanceRequest
+{
+    public Guid UserId { get; set; }
+    public Guid ScheduleId { get; set; }
+    public DateTimeOffset TimestampStart { get; set; }
+    public DateTimeOffset TimestampEnd { get; set; }
+}
