ddff9c23764ce733c907db2f39e2b177eaec72e1
Indrani <indranipandian@gmail.com>
2025-02-24 20:40:13 +0800

    Implement Web AttendanceService

---
+++ AttendEase.Web/Services/AttendanceService.cs
@@ -0,0 +1,22 @@
+﻿using AttendEase.DB.Contexts;
+using AttendEase.DB.Models;
+using AttendEase.Shared.Services;
+using AttendanceDBService = AttendEase.DB.Services.AttendanceService;
+
+namespace AttendEase.Web.Services;
+
+internal class AttendanceService(ILogger<AttendanceService> logger, AttendEaseDbContext context) : IAttendanceService
+{
+    private readonly ILogger<AttendanceService> _logger = logger;
+    private readonly AttendEaseDbContext _context = context;
+
+    public Task<Attendance?> GetAttendance(GetAttendanceRequest request, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        return AttendanceDBService.GetAttendance(request.UserId, request.ScheduleId, request.TimestampStart, request.TimestampEnd, _logger, _context, cancellationToken);
+    }
+}
