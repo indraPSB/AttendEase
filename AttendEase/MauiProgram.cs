d08ebe3f4cb38f6190ce4f0c444e52aacde5d050
Indrani <indranipandian@gmail.com>
2025-02-24 20:42:09 +0800

    Register MAUI AttendanceService

--- AttendEase/MauiProgram.cs
+++ AttendEase/MauiProgram.cs
@@ -59,6 +59,7 @@ namespace AttendEase
             });
 
             builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<IAttendanceService, AttendanceService>();
             builder.Services.AddScoped<ILocationService, LocationService>();
             builder.Services.AddScoped<IScheduleService, ScheduleService>();
             builder.Services.AddScoped<IUserService, UserService>();
