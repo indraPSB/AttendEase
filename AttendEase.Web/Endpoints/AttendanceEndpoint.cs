cd3edadf50e05b48e65515b808dcb2c936e792a2
Indrani <indranipandian@gmail.com>
2025-02-24 20:40:41 +0800

    Implement AttendanceEndpoint

---
+++ AttendEase.Web/Endpoints/AttendanceEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using AttendEase.DB.Models;
+using AttendEase.Shared.Models;
+using AttendEase.Shared.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AttendEase.Web.Endpoints;
+
+internal static class AttendanceEndpoint
+{
+    public static WebApplication MapAttendanceEndpoints(this WebApplication app)
+    {
+        app.MapGet("api/attendance", GetAttendance)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business},{UserRole.Standard}" });
+
+        return app;
+    }
+
+    private static async Task<IResult> GetAttendance([AsParameters] GetAttendanceRequest request, IAttendanceService attendanceService, CancellationToken cancellationToken)
+    {
+        Attendance? attendance = await attendanceService.GetAttendance(request, cancellationToken);
+
+        if (attendance is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(attendance);
+    }
+}
