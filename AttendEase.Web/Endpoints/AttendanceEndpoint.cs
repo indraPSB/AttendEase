52f39a2fd4b93aa76ebec6887520068e5805d9bc
Indrani <indranipandian@gmail.com>
2025-02-26 19:50:59 +0800

    Placeholder for mapping attendance endpoints

--- AttendEase.Web/Endpoints/AttendanceEndpoint.cs
+++ AttendEase.Web/Endpoints/AttendanceEndpoint.cs
@@ -9,16 +9,40 @@ internal static class AttendanceEndpoint
 {
     public static WebApplication MapAttendanceEndpoints(this WebApplication app)
     {
-        app.MapGet("api/attendance", GetAttendance)
+        app.MapGet("api/attendances", GetAttendances)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
+
+        app.MapGet("api/attendances/{id:guid}", GetAttendance)
             .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business},{UserRole.Standard}" });
 
-        app.MapPut("api/attendance", UpdateAttendance)
+        app.MapPost("api/attendances", AddAttendance)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
+
+        app.MapPut("api/attendances", UpdateAttendance)
             .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business},{UserRole.Standard}" });
+
+        app.MapDelete("api/attendances/{id:guid}", DeleteAttendance)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
+
+        app.MapPost("api/attendances/delete", DeleteAttendances)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
 
         return app;
     }
 
-    private static async Task<IResult> GetAttendance([AsParameters] GetAttendanceRequest request, IAttendanceService attendanceService, CancellationToken cancellationToken)
+    public static async Task<IResult> GetAttendances(IAttendanceService attendanceService, CancellationToken cancellationToken)
+    {
+        IEnumerable<Attendance>? attendances = null; //TODO: await attendanceService.GetAttendances(cancellationToken);
+
+        if (attendances is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(attendances);
+    }
+
+    public static async Task<IResult> GetAttendance([AsParameters] GetAttendanceRequest request, IAttendanceService attendanceService, CancellationToken cancellationToken)
     {
         Attendance? attendance = await attendanceService.GetAttendance(request, cancellationToken);
 
@@ -30,11 +54,44 @@ internal static class AttendanceEndpoint
         return Results.Ok(attendance);
     }
 
-    private static async Task<IResult> UpdateAttendance(Attendance attendance, IAttendanceService attendanceService, CancellationToken cancellationToken)
+    public static async Task<IResult> AddAttendance(Attendance attendance, IAttendanceService attendanceService, CancellationToken cancellationToken)
+    {
+        //TODO: if (await attendanceService.AddAttendance(attendance, cancellationToken))
+        if (false)
+        {
+            return Results.Ok(attendance);
+        }
+
+        return Results.BadRequest();
+    }
+
+    public static async Task<IResult> UpdateAttendance(Attendance attendance, IAttendanceService attendanceService, CancellationToken cancellationToken)
     {
         if (await attendanceService.UpdateAttendance(attendance, cancellationToken))
         {
             return Results.Ok(attendance);
+        }
+
+        return Results.BadRequest();
+    }
+
+    public static async Task<IResult> DeleteAttendance(Guid id, IAttendanceService attendanceService, CancellationToken cancellationToken)
+    {
+        //TODO: if (await attendanceService.DeleteAttendance(id, cancellationToken))
+        if (false)
+        {
+            return Results.Ok();
+        }
+
+        return Results.BadRequest();
+    }
+
+    public static async Task<IResult> DeleteAttendances(IEnumerable<Guid> ids, IAttendanceService attendanceService, CancellationToken cancellationToken)
+    {
+        //TODO: if (await attendanceService.DeleteAttendances(ids, cancellationToken))
+        if (false)
+        {
+            return Results.Ok();
         }
 
         return Results.BadRequest();
