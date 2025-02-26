354d64e8486b7e05f0873a991f71ce5962780769
Indrani <indranipandian@gmail.com>
2025-02-26 18:34:50 +0800

    Placeholder for mapping schedule endpoints

--- AttendEase.Web/Endpoints/ScheduleEndpoint.cs
+++ AttendEase.Web/Endpoints/ScheduleEndpoint.cs
@@ -12,8 +12,26 @@ internal static class ScheduleEndpoint
         app.MapGet("api/schedules", GetSchedules)
             .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
 
+        app.MapGet("api/schedules{id:guid}", GetSchedule)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
+
+        app.MapPost("api/schedules", AddSchedule)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
+
+        app.MapPut("api/schedules", UpdateSchedule)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
+
+        app.MapDelete("api/schedules/{id:guid}", DeleteSchedule)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
+
+        app.MapPost("api/schedules/delete", DeleteSchedules)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
+
         app.MapGet("api/schedules/user/{userId:guid}", GetSchedulesForUser)
             .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business},{UserRole.Standard}" });
+
+        app.MapPut("api/schedules/user/", UpdateUserAssignment)
+            .RequireAuthorization(new AuthorizeAttribute { Roles = $"{UserRole.Admin},{UserRole.Business}" });
 
         return app;
     }
@@ -30,6 +48,59 @@ internal static class ScheduleEndpoint
         return Results.Ok(schedules);
     }
 
+    public static async Task<IResult> GetSchedule(Guid id, IScheduleService scheduleService, CancellationToken cancellationToken)
+    {
+        Schedule? schedule = null; //await scheduleService.GetSchedule(id, cancellationToken);
+
+        if (schedule is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(schedule);
+    }
+
+    public static async Task<IResult> AddSchedule(Schedule schedule, IScheduleService scheduleService, CancellationToken cancellationToken)
+    {
+        //if (await scheduleService.AddSchedule(schedule, cancellationToken))
+        if (false)
+        {
+            return Results.Created($"/api/schedules/{schedule.Id}", schedule);
+        }
+
+        return Results.BadRequest();
+    }
+
+    public static async Task<IResult> UpdateSchedule(Schedule schedule, IScheduleService scheduleService, CancellationToken cancellationToken)
+    {
+        //if (await scheduleService.UpdateSchedule(schedule, cancellationToken))
+        if (false)
+        {
+            return Results.Ok(schedule);
+        }
+        return Results.BadRequest();
+    }
+
+    public static async Task<IResult> DeleteSchedule(Guid id, IScheduleService scheduleService, CancellationToken cancellationToken)
+    {
+        //if (await scheduleService.DeleteSchedule(id, cancellationToken))
+        if (false)
+        {
+            return Results.NoContent();
+        }
+        return Results.BadRequest();
+    }
+
+    public static async Task<IResult> DeleteSchedules(List<Guid> ids, IScheduleService scheduleService, CancellationToken cancellationToken)
+    {
+        //if (await scheduleService.DeleteSchedules(ids, cancellationToken))
+        if (false)
+        {
+            return Results.NoContent();
+        }
+        return Results.BadRequest();
+    }
+
     public static async Task<IResult> GetSchedulesForUser(Guid userId, IScheduleService scheduleService, CancellationToken cancellationToken)
     {
         IEnumerable<Schedule>? schedules = await scheduleService.GetSchedules(userId, cancellationToken);
@@ -40,5 +111,18 @@ internal static class ScheduleEndpoint
         }
 
         return Results.Ok(schedules);
+    }
+
+    public static async Task<IResult> UpdateUserAssignment(Schedule schedule, IScheduleService scheduleService, CancellationToken cancellationToken)
+    {
+        if (schedule is { Users: not null })
+        {
+            //if (await scheduleService.UpdateUserAssignment(schedule, cancellationToken))
+            if (false)
+            {
+                return Results.NoContent();
+            }
+        }
+        return Results.BadRequest();
     }
 }
