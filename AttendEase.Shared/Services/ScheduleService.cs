8711f0a5c60ab4de762a5b414e8d3ce8a155574b
Indrani <indranipandian@gmail.com>
2025-03-02 11:28:37 +0800

    Implement Shared ScheduleService

--- AttendEase.Shared/Services/ScheduleService.cs
+++ AttendEase.Shared/Services/ScheduleService.cs
@@ -8,6 +8,16 @@ public interface IScheduleService
 {
     Task<IEnumerable<Schedule>?> GetSchedules(CancellationToken cancellationToken = default);
 
+    Task<Schedule?> GetSchedule(Guid id, CancellationToken cancellationToken = default);
+
+    Task<bool> AddSchedule(Schedule schedule, CancellationToken cancellationToken = default);
+
+    Task<bool> UpdateSchedule(Schedule schedule, CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteSchedule(Guid id, CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteSchedules(IEnumerable<Guid> ids, CancellationToken cancellationToken = default);
+
     Task<IEnumerable<Schedule>?> GetSchedules(Guid userId, CancellationToken cancellationToken = default);
 }
 
@@ -49,6 +59,145 @@ public class ScheduleService(ILogger<ScheduleService> logger, HttpClient httpCli
         }
 
         return null;
+    }
+
+    public async Task<Schedule?> GetSchedule(Guid id, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"/api/schedules/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                Schedule? schedule = await response.Content.ReadFromJsonAsync<Schedule>(cancellationToken);
+
+                if (schedule is null)
+                {
+                    _logger.LogWarning("No schedule found.");
+                }
+                else
+                {
+                    _logger.LogInformation("Schedule retrieved successfully.");
+                }
+
+                return schedule;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in Shared GetSchedule(Guid) with message, '{message}'.", ex.Message);
+        }
+
+        return null;
+    }
+
+    public async Task<bool> AddSchedule(Schedule schedule, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        try
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("/api/schedules", schedule, cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogInformation("Schedule added successfully.");
+
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in Shared AddSchedule with message, '{message}'.", ex.Message);
+        }
+
+        return false;
+    }
+
+    public async Task<bool> UpdateSchedule(Schedule schedule, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        try
+        {
+            HttpResponseMessage response = await _httpClient.PutAsJsonAsync("/api/schedules", schedule, cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogInformation("Schedule updated successfully.");
+
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in Shared UpdateSchedule with message, '{message}'.", ex.Message);
+        }
+
+        return false;
+    }
+
+    public async Task<bool> DeleteSchedule(Guid id, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        try
+        {
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"/api/schedules/{id}", cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogInformation("Schedule deleted successfully.");
+
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in Shared DeleteSchedule with message, '{message}'.", ex.Message);
+        }
+
+        return false;
+    }
+
+    public async Task<bool> DeleteSchedules(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        try
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("/api/schedules/delete", ids, cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogInformation("Schedules deleted successfully.");
+
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in Shared DeleteSchedules with message, '{message}'.", ex.Message);
+        }
+
+        return false;
     }
 
     public async Task<IEnumerable<Schedule>?> GetSchedules(Guid userId, CancellationToken cancellationToken = default)
