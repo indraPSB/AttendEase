11071008c08c5ae03b18525dd4e03d2410c1712e
Indrani <indranipandian@gmail.com>
2025-03-02 11:29:50 +0800

    Implement Web ScheduleService

--- AttendEase.Web/Services/ScheduleService.cs
+++ AttendEase.Web/Services/ScheduleService.cs
@@ -22,13 +22,67 @@ internal class ScheduleService(ILogger<ScheduleService> logger, AttendEaseDbCont
         return ScheduleDBService.GetSchedules(_logger, _context, cancellationToken);
     }
 
-    public Task<IEnumerable<Schedule>?> GetSchedules(Guid id, CancellationToken cancellationToken = default)
+    public Task<Schedule?> GetSchedule(Guid id, CancellationToken cancellationToken = default)
     {
         if (cancellationToken == default)
         {
             cancellationToken = CancellationToken.None;
         }
 
-        return ScheduleDBService.GetSchedules(id, _logger, _context, cancellationToken);
+        return ScheduleDBService.GetSchedule(id, _logger, _context, cancellationToken);
+    }
+
+    public async Task<bool> AddSchedule(Schedule schedule, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        schedule.Id = Guid.CreateVersion7();
+
+        bool result = await ScheduleDBService.AddSchedule(schedule, _logger, _context, cancellationToken);
+
+        return result;
+    }
+
+    public Task<bool> UpdateSchedule(Schedule schedule, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        return ScheduleDBService.UpdateSchedule(schedule, _logger, _context, cancellationToken);
+    }
+
+    public Task<bool> DeleteSchedule(Guid id, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        return ScheduleDBService.DeleteSchedule(id, _logger, _context, cancellationToken);
+    }
+
+    public Task<bool> DeleteSchedules(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        return ScheduleDBService.DeleteSchedules(ids, _logger, _context, cancellationToken);
+    }
+
+    public Task<IEnumerable<Schedule>?> GetSchedules(Guid userId, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken == default)
+        {
+            cancellationToken = CancellationToken.None;
+        }
+
+        return ScheduleDBService.GetSchedules(userId, _logger, _context, cancellationToken);
     }
 }
